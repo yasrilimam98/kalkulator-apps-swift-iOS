48154fedb67ddc3544e7685eb2f3d8146708f457
Yasril Imam <gallerycreatifease@gmail.com>
2020-07-22 08:56:59 +0800

    push

--- kalkulator/ViewController.swift
+++ kalkulator/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heigth: UITextField!
+    @IBOutlet weak var widht: UITextField!
+    @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonresult(_ sender: UIButton) {
+        if let h = Double(heigth.text ?? "0"), let w = Double(widht.text ?? "0"){
+            result.text = String(h * w)
+        }else{
+            result.text = "Hasil Tidak Valid"
+        }
+    }
+    
+    @IBAction func resetView(_ sender: UIButton) {
+        result.text = ""
+        heigth.text = ""
+        widht.text = ""
+    }
 }
 
